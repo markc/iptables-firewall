0f6e66f136b78ddbb0479ca91c2bc2642ed399fe
Hans Freitag <zem@fnordpol.de>
2017-08-24 00:00:16 +0200

    added README

---
+++ README.mk
@@ -0,0 +1,17 @@
+Run ./install.sh for setup
+
+you may need to configure the location of iptables and ip6tables in fw-lib.sh 
+if you are not using debian stretch with bash
+
+place your firewall scripts in /etc/fw_rules.d
+
+Example: 
+
+--------------------------------------------------
+#!/usr/lib/fw-lib.sh
+require RULE-FILE-IN-ETC-FW_RULES.D
+iptables -N SOMECHAIN
+iptables -I SOMECHAIN -j ACCEPT 
+ip6tables -I INPUT -j REJECT
+-------------------------------------------------
+
