7d373df06723bd2fd4c88947e11f9ee1df8cafb6
Hans Freitag <zem@fnordpol.de>
2018-03-12 22:40:25 +0100

    install some extra dirs

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ PREFIX=${PREFIX:-}
 INSTDIR=${INSTDIR:-/usr/local}
 echo $PREFIX$INSTDIR
 exit 
+install -d ${PREFIX}${INSTDIR}/share
+install -d ${PREFIX}${INSTDIR}/sbin
 install  -m "0755" -o root -g root firewall.service ${PREFIX}/etc/systemd/system/firewall.service
 install  -m "0755" -o root -g root fw-lib.sh ${PREFIX}${INSTDIR}/share/fw-lib.sh
 install  -m "0755" -o root -g root fw-load.sh ${PREFIX}${INSTDIR}/sbin/fw-load.sh
